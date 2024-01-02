33c63fc5266929593569490637d8f430c3b88b0b
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2024-01-02 02:39:08 +0000

    basic fundamentals learning and practice in c++

--- test.cpp
+++ test.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    a = 12;
+    cout << "size of int = " << sizeof(a) << endl;
 
+    float b;
+    b = 100.10;
+    cout << "size of float = " << sizeof(b) << endl;
+
+    bool c;
+    c = true;
+    cout << "Bool size of = " << sizeof(c) << endl;
+
+    char firstLetter = 'A';
+    cout << "The firstletter size is = " << sizeof(firstLetter) << endl;
+    return 0;
+}
