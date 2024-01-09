337df944005e4376a26e54201d6e1c17c361ffa1
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2024-01-09 14:54:20 +0000

    operator practice

---
+++ operator.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    // int numOne = 10;
+    // cout << numOne << endl;
+
+    // numOne++;
+    // cout << numOne << endl;
+
+    // ++numOne;
+    // cout << numOne << endl;
+
+    // int numTwo = numOne++;
+    // numTwo = ++numOne;
+    // cout<<numTwo<<endl;
+
+
+// int numOne=10, numTwo=10;
+// if(numOne==numTwo){
+//     cout<<"Numone equal to numTwo\n";
+// }
+
+}
