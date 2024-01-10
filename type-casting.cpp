8c69a8f539c5e0fd4f5e382709de8581b94278ac
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2024-01-10 02:11:50 +0000

    type casting practice

---
+++ type-casting.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    // ===== type casting ====
+    // int numOne=10;
+    // cout<<sizeof(numOne)<<endl;
+    // char(numOne);
+    // char numTwo = char(numOne);
+    // cout<<sizeof(numTwo)<<endl;
+
+    // int a=10;
+    // char b='b'; //implicit/auto conversion
+    //  a = a+b;
+    // cout<<a<<endl;
+
+    // int numOne = 10;
+    // float numTwo = 1.5;
+    // numOne = numOne + numTwo;
+    // cout << numOne << endl;
+
+    // int numOne=10;
+    // float numTwo = 1.9;
+    // int sumNum = numOne + (int)numTwo;
+    // cout<<sumNum<<endl;
+
+    // float numOne=2.198;
+    // int newType = static_cast<int>(numOne);
+    // cout<<newType<<endl;
+
+    // int numTwo = 100;
+    // float newFloatNum = static_cast<float>(numTwo);
+    // cout<<newFloatNum<<endl;
+
+    // int numThree = 10;
+    // char *newCastType = dynamic_cast<&char>(numThree); //need learning pointer in c++ 
+    // cout<<newCastType<<endl;
+}
