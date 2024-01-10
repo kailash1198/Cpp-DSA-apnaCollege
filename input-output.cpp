5e72b3d313fd55deacafed1e902ddd8807b6ca55
Kailash kumar <79510867+kailash1198@users.noreply.github.com>
2024-01-10 02:24:08 +0000

    i/o practice

--- input-output.cpp
+++ input-output.cpp
@@ -3,15 +3,18 @@ using namespace std;
 
 int main()
 {
-    int momAmount;
-    int papaAmount;
-    int totalAmount;
-    cout << "Please enter Given by MOM anount: ";
-    cin >> momAmount;
-    cout << "Please enter Given by PAPA anount: ";
-    cin >> papaAmount;
-    totalAmount = momAmount + papaAmount;
-    cout << "Total amount is ";
-    cout << totalAmount;
+    // int momAmount;
+    // int papaAmount;
+    // int totalAmount;
+    // cout << "Please enter Given by MOM anount: ";
+    // cin >> momAmount;
+    // cout << "Please enter Given by PAPA anount: ";
+    // cin >> papaAmount;
+    // totalAmount = momAmount + papaAmount;
+    // cout << "Total amount is ";
+    // cout << totalAmount;
+
+    // int numOne=100;
+    // cerr<<"Found error in code"<<endl;
     return 0;
 }
