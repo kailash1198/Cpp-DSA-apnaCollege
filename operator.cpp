4914d484d46b5a6c3d7b79c3322bfc2ce9f1d3a5
kailash kumar <kailash414243@gmail.com>
2024-01-18 21:25:31 +0530

    Remote folder checking...

--- operator.cpp
+++ operator.cpp
@@ -16,10 +16,18 @@ int main(void)
     // numTwo = ++numOne;
     // cout<<numTwo<<endl;
 
+    // int numOne=10, numTwo=10;
+    // if(numOne==numTwo){
+    //     cout<<"Numone equal to numTwo\n";
+    // }
 
-// int numOne=10, numTwo=10;
-// if(numOne==numTwo){
-//     cout<<"Numone equal to numTwo\n";
-// }
-
+    int numOne = 100, numTwo = 200;
+    if (numOne && numTwo == 0)
+    {
+        cout << "Numone and numTwo = 0\n";
+    }
+    else
+    {
+        cout << "Not equal to 0\n";
+    }
 }
